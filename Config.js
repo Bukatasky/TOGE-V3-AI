1747d7cf8cfdb46a7aff956e34df767df04744f1
Bukatasky <165413858+Bukatasky@users.noreply.github.com>
2025-01-01 08:14:17 +0200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //contact details
-global.ownernumber = [ process.env.OWNER_NUMBER || "24105114159"],
-global.ownername = process.env.OWNER_NAME || "𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈",
+global.ownernumber = [ process.env.OWNER_NUMBER || "263716616101"],
+global.ownername = process.env.OWNER_NAME || "SKYWRLD ",
 global.ytname = "YT: kenzo3146"
 global.socialm = "GitHub: toge012345"
 global.location = "Africa"
@@ -11,9 +11,9 @@ global.location = "Africa"
 global.botname = process.env.BOTNAME || "𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑",//name of the bot
 
 //sticker details
-global.stickername = process.env.STICKER || "𝐓𝐎𝐆𝐄-𝐌𝐃-𝐕𝟑",
+global.stickername = process.env.STICKER || "SKYWRLD",
 global.packname = 'Sticker By'
-global.author = process.env.AUTHOR || "𓆩𝐓𝐎𝐆𝐄𓆪 ✇ ◤✞𝐈𝐍𝐔𝐌𝐀𝐊𝐈",
+global.author = process.env.AUTHOR || "BUKATASKY ",
 //console view/theme
 global.themeemoji = '𓅃'
 global.wm = "𝐓𝐎𝐆𝐄_𝐁𝐎𝐓 𝐈𝐧𝐜 ✨"
@@ -22,7 +22,7 @@ global.wm = "𝐓𝐎𝐆𝐄_𝐁𝐎𝐓 𝐈𝐧𝐜 ✨"
 global.link = 'https://whatsapp.com/channel/0029VaiuD4s4IBhI0fzbv40Z'
 
 //custom prefix
-global.prefa = process.env.PREFIX || ".",
+global.prefa = process.env.PREFIX || ",",
 
 //false=disable and true=enable
 global.welcome = process.env.WELCOME || "false", //auto welcome
